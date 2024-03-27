09ccbc624effe3e5ba19c7970bc627a6992c0251
ZunaishaN00R <zunaishanoor1234@gmail.com>
2024-03-27 17:21:52 +0500

    first commit

---
+++ SRC/dummydata.js
@@ -0,0 +1,393 @@
+export const homeAbout = [
+  {
+    id: 1,
+    cover: "https://img.icons8.com/dotty/80/000000/storytelling.png",
+    title: "Online Courses",
+    desc: "Explore a wide range of online courses at Superior University.",
+  },
+  {
+    id: 1,
+    cover: "https://img.icons8.com/ios/80/000000/diploma.png",
+    title: "Earn A Certificates",
+    desc: "Earn certificates and showcase your achievements at Superior University.",
+  },
+  {
+    id: 1,
+    cover: "https://img.icons8.com/ios/80/000000/athlete.png",
+    title: "Learn with Expert",
+    desc: "Learn from industry experts and enhance your skills at Superior University.",
+  },
+]
+export const awrapper = [
+  {
+    cover: "https://img.icons8.com/external-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto/80/ffffff/external-graduation-education-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto.png",
+    data: "3,000",
+    title: "SUCCESS STORIES",
+  },
+
+  {
+    cover: "https://img.icons8.com/ios/80/ffffff/athlete.png",
+    data: "320",
+    title: "TRUSTED TUTORS",
+  },
+  {
+    cover: "https://img.icons8.com/external-outline-icons-maxicons/80/ffffff/external-calender-insurance-outline-outline-icons-maxicons.png",
+    data: "1,000",
+    title: "SCHEDULES",
+  },
+  {
+    cover: "https://img.icons8.com/ios/80/ffffff/macbook-idea--v3.png",
+    data: "587",
+    title: "COURSES",
+  },
+]
+export const coursesCard = [
+  {
+    id: 1,
+    cover: "../images/courses/c1.png",
+    coursesName: "Web Engineering 1",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by John Smith",
+        totalTime: "30 lectures (190 hrs)",
+      },
+    ],
+    priceAll: "$100 All Course",
+    pricePer: "$15 per month",
+  },
+  {
+    id: 2,
+    cover: "../images/courses/c2.png",
+    coursesName: "Introduction to Computing ",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Ram Gurung",
+        totalTime: "30 lectures (125 hrs)",
+      },
+    ],
+    priceAll: "$200 All Course",
+    pricePer: "$25 per month",
+  },
+  {
+    id: 3,
+    cover: "../images/courses/c3.png",
+    coursesName: "Programming Fundamentals ",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Saroj Nepal",
+        totalTime: "50 lectures (50 hrs)",
+      },
+    ],
+    priceAll: "$50 All Course",
+    pricePer: "$5 per month",
+  },
+  {
+    id: 4,
+    cover: "../images/courses/c4.png",
+    coursesName: "Basic Electronics",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Prabin Thapa Mager",
+        totalTime: "20 lectures (20 hrs)",
+      },
+    ],
+    priceAll: "$30 All Course",
+    pricePer: "$3 per month",
+  },
+  {
+    id: 5,
+    cover: "../images/courses/c5.png",
+    coursesName: "Object-Oriented Programming",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Sunil Bishowkaram",
+        totalTime: "100 lectures (150 hrs)",
+      },
+    ],
+    priceAll: "$300 All Course",
+    pricePer: "$30 per month",
+  },
+  {
+    id: 6,
+    cover: "../images/courses/c6.png",
+    coursesName: "Computer Applications",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Sunil Bishowkaram",
+        totalTime: "200 lectures (300 hrs)",
+      },
+    ],
+    priceAll: "$500 All Course",
+    pricePer: "$80 per month",
+  },
+  {
+    id: 7,
+    cover: "../images/courses/c7.png",
+    coursesName: "Probability and Statistics",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Price",
+        totalTime: "20 lectures (50 hrs)",
+      },
+    ],
+    priceAll: "$20 All Course",
+    pricePer: "$2 per month",
+  },
+  {
+    id: 8,
+    cover: "../images/courses/c8.png",
+    coursesName: "Data Structures and Algorithms",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Petter",
+        totalTime: "80 lectures (200 hrs)",
+      },
+    ],
+    priceAll: "$200 All Course",
+    pricePer: "$40 per month",
+  },
+  {
+    id: 9,
+    cover: "../images/courses/c9.png",
+    coursesName: "Digital Logic Design ",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Petter",
+        totalTime: "50 lectures (50 hrs)",
+      },
+    ],
+    priceAll: "$20 All Course",
+    pricePer: "$3 per month",
+  },
+]
+export const online = [
+  {
+    cover: "./images/courses/online/o1.png",
+    hoverCover: "./images/courses/online/o1.1.png",
+    courseName: "Database Systems ",
+    course: "25 Courses",
+  },
+  {
+    cover: "./images/courses/online/o2.png",
+    hoverCover: "./images/courses/online/o2.1.png",
+    courseName: "Microprocessor and Microcontroller Based Systems ",
+    course: "25 Courses",
+  },
+  {
+    cover: "./images/courses/online/o3.png",
+    hoverCover: "./images/courses/online/o3.1.png",
+    courseName: "Artificial Intelligence ",
+    course: "10 Courses",
+  },
+  {
+    cover: "./images/courses/online/o4.png",
+    hoverCover: "./images/courses/online/o4.1.png",
+    courseName: "Data Communication & Computer Networks ",
+    course: "15 Courses",
+  },
+  {
+    cover: "./images/courses/online/o5.png",
+    hoverCover: "./images/courses/online/o5.1.png",
+    courseName: "Software Engineering",
+    course: "30 Courses",
+  },
+  {
+    cover: "./images/courses/online/o6.png",
+    hoverCover: "./images/courses/online/o6.1.png",
+    courseName: "Web Engineering",
+    course: "60 Courses",
+  },
+  {
+    cover: "./images/courses/online/o7.png",
+    hoverCover: "./images/courses/online/o7.1.png",
+    courseName: "Cloud Computing",
+    course: "10 Courses",
+  },
+  {
+    cover: "./images/courses/online/o8.png",
+    hoverCover: "./images/courses/online/o8.1.png",
+    courseName: "Operating System",
+    course: "30 Courses",
+  },
+  {
+    cover: "./images/courses/online/o9.png",
+    hoverCover: "./images/courses/online/o9.1.png",
+    courseName: "Senior Design Project ",
+    course: "80 Courses",
+  },
+  {
+    cover: "./images/courses/online/o10.png",
+    hoverCover: "./images/courses/online/o10.1.png",
+    courseName: "Enterprenureship",
+    course: "120 Courses",
+  },
+  {
+    cover: "./images/courses/online/o11.png",
+    hoverCover: "./images/courses/online/o11.1.png",
+    courseName: "Mobile App Developement",
+    course: "17 Courses",
+  },
+  {
+    cover: "./images/courses/online/o12.png",
+    hoverCover: "./images/courses/online/o12.1.png",
+    courseName: "Computer Vision",
+    course: "17 Courses",
+  },
+]
+export const team = [
+  {
+    cover: "./images/team/t2.webp",
+    name: "Anas Abdullah",
+    email: "biet-f21-006@superior.edu.pk",
+    work: "WEB DEVELOPER",
+  },
+  {
+    cover: "./images/team/t3.webp",
+    name: "Bisma Qamar",
+    email: "biet-f21-004@superior.edu.pk",
+    work: "WEB DEVELOPER",
+  },
+  {
+    cover: "./images/team/t3.webp",
+    name: "Khuzaima Faizan",
+    email: "biet-f21-047@superior.edu.pk",
+    work: "WEB DEVELOPER",
+  },
+  {
+    cover: "./images/team/t3.webp",
+    name: "Zunaisha Noor",
+    email: "biet-f21-049@superior.edu.pk",
+    work: "WEB DEVELOPER",
+  },
+]
+export const price = [
+  {
+    name: "BASIC PLAN",
+    price: "49K",
+    desc: "Unlock the essentials with our Basic Plan, tailored to meet your fundamental needs for a seamless experience.",
+  },
+  {
+    name: "BEGINNER PLAN",
+    price: "79K",
+    desc: "Embark on your journey with the Beginner Plan, offering enhanced features to kickstart your endeavors with confidence.",
+  },
+  {
+    name: "PREMIUM PLAN",
+    price: "109k",
+    desc: "Take your projects to the next level with our Premium Plan, designed to provide advanced tools and resources for professional growth.",
+  },
+  {
+    name: "ULTIMATE PLAN",
+    price: "149K",
+    desc: "Experience the pinnacle of performance with our Ultimate Plan, delivering unmatched capabilities and exclusive benefits for unparalleled success.",
+  },
+]
+export const faq = [
+  {
+    title: "How Can I Access and Enroll in the Online Courses?",
+    desc: "To enroll in our online courses, you can access the course catalog on our website. Click on the desired course, and you will find enrollment details and steps on the course page. Follow the instructions to complete the enrollment process.",
+  },
+  {
+    title: "What is the Default Hidden State Until the Collapse?",
+    desc: "The default hidden state until the collapse is a feature provided by the collapse plugin. This functionality adds specific classes to style elements, making them initially hidden. It's important to note that you can embed various HTML within the .accordion-body, but the transition will limit overflow.",
+  },
+  {
+    title: "Can You Explain the Default Hidden State Until the Collapse?",
+    desc: "The default hidden state until the collapse is a design element enabled by the collapse plugin. This feature introduces specific classes for styling, ensuring elements start in a hidden state. While you can include diverse HTML content within the .accordion-body, please be mindful of the transition, which restricts overflow.",
+  },
+  {
+    title: "Is There a Step-by-Step Guide for Enrolling in Online Courses?",
+    desc: "Certainly! We provide a detailed step-by-step guide to help you enroll in our online courses. Visit our website, navigate to the course of interest, and find the enrollment section. Follow the outlined steps to complete the enrollment process hassle-free.",
+  },
+  {
+    title: "What Happens Behind the Scenes in the Default Hidden State?",
+    desc: "The default hidden state, implemented through the collapse plugin, involves the addition of specific classes for styling. This ensures that elements initially remain hidden, providing a clean and organized appearance. Keep in mind that you can include various HTML content within the .accordion-body, but the transition effect limits overflow.",
+  },
+  {
+    title: "Could You Elaborate on the Default Hidden State Until the Collapse?",
+    desc: "Certainly! The default hidden state until the collapse is achieved through the collapse plugin's classes. These classes are responsible for styling elements to remain hidden initially. It's essential to note that you have the flexibility to include diverse HTML content within the .accordion-body, but be mindful of the transition's impact on overflow.",
+  },
+];
+export const blog = [
+  {
+    id: 1,
+    type: "admin",
+    date: "MAR. 10, 2023",
+    com: "4 COMMENTS",
+    title: "Unleash Your Potential in Software Development",
+    desc: "Discover the endless possibilities in the world of software development. Dive into the journey of building innovative solutions and shaping the future of technology.",
+    cover: "../images/blog/b1.webp",
+  },
+  {
+    id: 2,
+    type: "admin",
+    date: "APR. 05, 2023",
+    com: "6 COMMENTS",
+    title: "Mastering the Art of Software Engineering",
+    desc: "Embark on a quest to master the art of software engineering. Explore the principles, practices, and tools that empower software developers to create robust and scalable solutions.",
+    cover: "../images/blog/b2.webp",
+  },
+  {
+    id: 3,
+    type: "user",
+    date: "JUN. 20, 2023",
+    com: "15 COMMENTS",
+    title: "Crafting Your Path in the World of Programming",
+    desc: "Forge your unique path in the vast world of programming. Learn how to navigate challenges, embrace creativity, and build a fulfilling career in software development.",
+    cover: "../images/blog/b3.webp",
+  },
+  {
+    id: 4,
+    type: "admin",
+    date: "MAR. 02, 2023",
+    com: "5 COMMENTS",
+    title: "Exploring Emerging Trends in Software Design",
+    desc: "Stay ahead of the curve by exploring the latest trends in software design. From user experience to architectural patterns, delve into the evolving landscape of software development.",
+    cover: "../images/blog/b4.webp",
+  },
+  {
+    id: 5,
+    type: "admin",
+    date: "NOV. 18, 2023",
+    com: "7 COMMENTS",
+    title: "Innovative Solutions for Tomorrow's Challenges",
+    desc: "Join the journey of creating innovative solutions to address the challenges of tomorrow. Learn about cutting-edge technologies and methodologies shaping the future of software engineering.",
+    cover: "../images/blog/b5.webp",
+  },
+  {
+    id: 6,
+    type: "user",
+    date: "MAR. 10, 2023",
+    com: "18 COMMENTS",
+    title: "Empowering Your Coding Journey",
+    desc: "Empower your coding journey with insights, tips, and resources to enhance your skills. Uncover the secrets of successful developers and navigate the ever-evolving world of coding.",
+    cover: "../images/blog/b6.webp",
+  },
+];
+export const testimonal = [
+  {
+    id: 1,
+    name: "Miss Fatima Zafar",
+    post: "LAB          INSTRUCTOR",
+    email:"fatima.zafar@superior.edu.pk",
+    desc: "With unwavering dedication and a strong work ethic,goes above and beyond to create an engaging learning environment. Her commitment to excellence is a guiding light for students on their educational journey.",
+    cover: "./images/testo/Fatima-Zafar.webp",
+  },
+  {
+    id: 2,
+    name: "Miss Kinza Fayyaz",
+    post: "COURSE INTSTRUCTOR",
+    email:"kinza.fayyaz@superior.edu.pk",
+    desc: "She is a beacon of dedication and hard work. With a passion for teaching and a tireless commitment to student success, she navigates the educational landscape with enthusiasm. Her hard work is an inspiration to all who seek knowledge under her guidance.",
+    cover: "./images/testo/hijab.jpg",
+  },
+]
